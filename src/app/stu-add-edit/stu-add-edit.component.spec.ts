0d0b59acd6a6add966abc34712e6c9f41cb40b2c
HoaDP2001 <135573801+HoaDP2001@users.noreply.github.com>
2024-03-25 09:52:37 +0700

    9:52 AM

---
+++ src/app/stu-add-edit/stu-add-edit.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StuAddEditComponent } from './stu-add-edit.component';
+
+describe('StuAddEditComponent', () => {
+  let component: StuAddEditComponent;
+  let fixture: ComponentFixture<StuAddEditComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StuAddEditComponent]
+    });
+    fixture = TestBed.createComponent(StuAddEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/stu-add-edit/stu-add-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stu-add-edit',
+  templateUrl: './stu-add-edit.component.html',
+  styleUrls: ['./stu-add-edit.component.scss']
+})
+export class StuAddEditComponent {
+
+}
